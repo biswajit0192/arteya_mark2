0b0b47b1dbcb663ca6318b4d957a532df8d4e31c
biswajit0192 <mohanty12biswajit@gmail.com>
2022-02-09 14:02:23 +0530

    Main

--- res/js/main.js
+++ res/js/main.js
@@ -9,3 +9,21 @@ menuIcon.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
     mobMenu.style.display = 'none';
 });
+
+////////// To Top Btn ////////////
+const topBtn = document.getElementById('to-top-btn');
+const htmlbody = document.getElementsByTagName("body");
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= 800) {
+        topBtn.style.opacity = "1";
+    }
+    else {
+        topBtn.style.opacity = "0";
+    }
+})
+
+topBtn.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+    // htmlbody.style.scrollBehavior = "smooth";
+});
